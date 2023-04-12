9d80c43a7128142868ba29dfef498161793ca480
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-04-12 14:52:39 -0500

    it is best to use let rather than var keyword

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["buy food", "Cook food", "eat food"]; // collection of items
+let items = ["buy food", "Cook food", "eat food"]; // collection of items
 app.set("view engine", "ejs"); // setting view engine
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 // post home route
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
