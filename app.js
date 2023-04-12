f17eab99999319dae5053bc1fd08a6161954cff8
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-04-12 10:38:29 -0500

    Using EJS templates

--- app.js
+++ app.js
@@ -2,15 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs"); // setting view engine
 
 app.get("/", function (req, res) {
   var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Yay! it is the weekend!");
-  } else {
-    res.sendFile(__dirname + "/index.html");
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+
+    default:
+      console.log("Error: Current day is equal to: " + currentDay);
   }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
